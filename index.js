76d5095884622b06d00d12d07f22156570ef3537
Neptunus <neptunus@hirt.is>
2019-04-06 14:14:09 +0000

    text input for hue, tweaking colors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {P, S, PS, D} from 'patchinko/explicit.mjs';
 
 const app = {
 	initialState: {
-		hue: 0,
+		hue: 180,
 	},
 	actions: update => {
 		return {
@@ -17,14 +17,14 @@ const app = {
 
 const Color = {
 	view: function({ attrs: { state, offset = 0 } }) {
-		const color = `hsl(${ state.hue }, ${40 + Math.round(offset*0.8) }%, ${50 + offset }%)`;
-		return m( 'div.flex.items-center',
-			m('div.px-4.py-6.text-white.w-64.text-center', {
-				style: `background-color: ${color}`,
+    const color = `hsl(${ state.hue % 360 }, ${40 - Math.round(offset*0.4) }%, ${50 + offset }%)`;
+		return m( '.flex.items-center',
+			m('.px-4.py-6.w-48.text-center.flex-none.tabular-nums', {
+				style: `background-color: ${color}; color: ${offset > 0 ? 'black' : 'white'}`,
 				},
 				color
 			),
-			m('div.border.rounded.px-4.py-2.ml-6', {
+			m('.border.rounded.px-4.py-2.ml-6.ml-3.md:flex-grow-0.flex-grow.text-center', {
 				style: `border-color: ${color}; color: ${color}`,
 				},
 				`offset: ${offset}`
@@ -35,21 +35,28 @@ const Color = {
 
 const App = {
 	view: function({ attrs: { state, actions } }) {
-		return m('.m-8',
-			m('input.mb-4[type=range][max=360]',
-				{
-					value: state.hue,
-					oninput: (e)=> actions.modifyHue(Number(e.target.value))
-				}
-			),
-			m('label.ml-6', state.hue ),
-			m(Color, { state: state, offset: -24 }),
-			m(Color, { state: state, offset: -16 }),
-			m(Color, { state: state, offset: -8 }),
+		return m('.m-3',
+      m('',
+        m('input.mb-4.ml-6.w-40[type=range][max=359]',
+          {
+            value: state.hue % 360,
+            oninput: (e)=> actions.modifyHue(Number(e.target.value))
+          }
+        ),
+        m('input.ml-6.w-32.border.px-3[type=number][pattern="[0-9]*"]', {
+          value: state.hue,
+          oninput: (e) => actions.modifyHue(Number(e.target.value))
+        }),
+      ),
+      m(Color, { state: state, offset: -36 }),
+      m(Color, { state: state, offset: -30 }),
+			m(Color, { state: state, offset: -20 }),
+			m(Color, { state: state, offset: -10 }),
 			m(Color, { state: state }),
-			m(Color, { state: state, offset: 12 }),
-			m(Color, { state: state, offset: 24 }),
-			m(Color, { state: state, offset: 36 }),
+			m(Color, { state: state, offset: 10 }),
+			m(Color, { state: state, offset: 20 }),
+      m(Color, { state: state, offset: 30 }),
+			m(Color, { state: state, offset: 40 }),
 			// m('pre', JSON.stringify(state, null, 2) )
 		)
 	}
